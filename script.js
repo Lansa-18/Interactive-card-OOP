143721874026171aaba15672f1ab8f98e80bb1a7
Mapelujo <olamideiyanda18@gmail.com>
2023-12-22 10:47:13 +0100

    progress

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Getting Elements from HTML
+const inputCardholder = document.querySelector('#card-name');
+const inputCardNumber = document.querySelector('#card-number');
+const inputCardMonth = document.querySelector('.card-month');   
+const inputCardYear = document.querySelector('.card-year');
+const inputCardCvc = document.querySelector('#card-cvc');
+console.log('Hello World');
